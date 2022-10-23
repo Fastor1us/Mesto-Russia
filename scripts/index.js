eba36755415e30136973b969bfa8292cae63d381
Oleg Kiryushin <fewgwer3@yandex.ru>
2022-10-23 04:07:38 +0300

    refactor, fix - правки после ревью и подготовка кода к новому спринту

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,34 @@
-const popup = document.querySelector('.popup')
+/********************   DATA   ********************/
 
-document.querySelector('.profile__edit-button').addEventListener('click',
+const popup = document.querySelector('.popup')
+const popupInputs = popup.querySelectorAll('.popup__text-input')
+const popupConfirmButton = popup.querySelector('.popup__confirm-button')
+const profileTitle = document.querySelector('.profile__title')
+const profileSubtitle = document.querySelector('.profile__subtitle')
+const profileEditButton = document.querySelector('.profile__edit-button')
+const profileAddButton = document.querySelector('.profile__add-button')
+
+/********************   ACTIONS   ********************/
+
+profileEditButton.addEventListener('click',
   function () {
-    if (!popup.classList.contains('popup_opened')) {
-      popup.classList.add('popup_opened')
-    }
+    openPopup()
+    popup.querySelector('.popup__title').textContent = 'Редактировать профиль'
+    popupInputs[0].value = profileTitle.textContent
+    popupInputs[0].placeholder = 'Имя'
+    popupInputs[1].value = profileSubtitle.textContent
+    popupInputs[1].placeholder = 'Описание'
+    popupConfirmButton.textContent = 'Сохранить'
   }
 )
 
-document.querySelector('.popup__close-button').addEventListener('click',
+profileAddButton.addEventListener('click',
+  function () {
+    openPopup()
+  }
+)
+
+popup.querySelector('.popup__close-button').addEventListener('click',
   function () {
     if (popup.classList.contains('popup_opened')) {
       popup.classList.remove('popup_opened')
@@ -16,11 +36,26 @@ document.querySelector('.popup__close-button').addEventListener('click',
   }
 )
 
+popup.querySelector('.popup__container').addEventListener('submit',
+  function (EventTarget) {
+    EventTarget.preventDefault()
+    profileTitle.textContent = popupInputs[0].value
+    profileSubtitle.textContent = popupInputs[1].value
+    if (popup.classList.contains('popup_opened')) {
+      popup.classList.remove('popup_opened')
+    }
+  }
+)
+
 document.querySelectorAll('.cards__button-like').forEach(
-    function(item) {
-      item.addEventListener('click',
-      function () {
-        item.blur()
+  function(item) {
+    item.addEventListener('click',
+    function () {
+      item.blur()
     })
   }
-  )
+)
+
+/********************   FUNCTIONS   ********************/
+
+function openPopup () { popup.classList.add('popup_opened') }
