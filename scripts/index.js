dd5a66bbe6d252562635c79163c1c042065e8f9e
Oleg Kiryushin <fewgwer3@yandex.ru>
2022-10-23 05:49:43 +0300

    refactor: создание и пуш ветки dev

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,16 @@
 /********************   DATA   ********************/
+
 const popup = document.querySelector('.popup')
 const popupInputs = popup.querySelectorAll('.popup__text-input')
 const popupCloseButton = popup.querySelector('.popup__close-button')
 const popupTitle = popup.querySelector('.popup__title')
 const popupConfirmButton = popup.querySelector('.popup__confirm-button')
+const popupContainer = popup.querySelector('.popup__container')
 const profileTitle = document.querySelector('.profile__title')
 const profileSubtitle = document.querySelector('.profile__subtitle')
 const profileEditButton = document.querySelector('.profile__edit-button')
 const profileAddButton = document.querySelector('.profile__add-button')
+const cardsContainer = document.querySelector('.cards__list')
 
 /********************   ACTIONS   ********************/
 
@@ -33,19 +36,17 @@ profileAddButton.addEventListener('click', () => {
   }
 )
 
-popupCloseButton.addEventListener('click', () => {
-    popup.classList.remove('popup_opened')
-  }
-)
-
-popup.querySelector('.popup__container').addEventListener('submit',
-  function (EventTarget) {
+popupContainer.addEventListener('submit', (EventTarget) => {
     EventTarget.preventDefault()
-    profileTitle.textContent = popupInputs[0].value
-    profileSubtitle.textContent = popupInputs[1].value
-    if (popup.classList.contains('popup_opened')) {
-      popup.classList.remove('popup_opened')
+    switch (popupTitle.textContent) {
+      case 'Редактировать профиль':
+        profileTitle.textContent = popupInputs[0].value
+        profileSubtitle.textContent = popupInputs[1].value
+        break
+      case 'Новое место':
+        addCard()
     }
+    closePopup()
   }
 )
 
@@ -58,6 +59,23 @@ document.querySelectorAll('.cards__button-like').forEach(
   }
 )
 
+popupCloseButton.addEventListener('click', () => {
+  closePopup()
+}
+)
+
 /********************   FUNCTIONS   ********************/
 
 function openPopup () { popup.classList.add('popup_opened') }
+function closePopup () { popup.classList.remove('popup_opened') }
+
+function addCard () {
+  const cardTemplate = document.querySelector('#card-template').content
+  const cardElement = cardTemplate.querySelector('.cards__item').cloneNode(true)
+
+  cardElement.querySelector('.cards__image').setAttribute('src', popupInputs[1].value)
+  cardElement.querySelector('.cards__image').setAttribute('alt', popupInputs[0].value)
+  cardElement.querySelector('.cards__title').textContent = popupInputs[0].value
+
+  cardsContainer.prepend(cardElement)
+}
